1b0b2894a733477c7eb43428de8ab88518d83b31
Agimir <william@mesaindigostudios.com>
2023-08-09 14:55:31 -0500

    added to stream

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,6 +14,7 @@ description 'For support with streaming assets pertaining to M.I. Resources'
 -- dependencies
 dependencies {
     'ox_inventory',
+    'ox_lib',
     'ox_target'
 }
 
@@ -38,40 +39,6 @@ server_scripts {
     'server/main.lua'
 }
 
--- stream
 files {
--- props
-    'stream/**/**/*.ytyp',
-    'stream/**/**/*.ymf',
-    'stream/**/**/*.ydr',
--- vehicles
-    'stream/**/**/*.ytd',
-    'stream/**/**/*.yft',
-    'stream/**/**/vehicles.meta',
-    'stream/**/**/carvariations.meta',
-    'stream/**/**/carcols.meta',
-    'stream/**/**/handling.meta',
-    'stream/**/**/vehiclelayouts.meta',
-    'stream/**/**/dlctext.meta',
-    'stream/**/**/contentunlocks.meta',
-
+    'web/peds/*.png'
 }
-
--- vehicles
-data_file 'HANDLING_FILE' 'stream/**/**/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/**/**/vehicles.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/**/**/vehiclelayouts.meta'
-data_file 'CARCOLS_FILE' 'stream/**/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'stream/**/**/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'stream/**/**/dlctext.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/**/**/contentunlocks.meta'
-data_file 'AMBIENT_PED_MODEL_SET_FILE' 'stream/**/**/ambientpedmodelsets.meta'
--- props
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/badges.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_effect_cigarpack.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_food_dessert_a.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_food_icecream_pack.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_food_xmas22.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_foodpack.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/dingus.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/**/**/knjgh_pizzas.ytyp'
