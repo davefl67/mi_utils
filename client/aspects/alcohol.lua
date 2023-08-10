7404ba070a8de85572b977afaa91a7461191d7fe
Agimir <william@mesaindigostudios.com>
2023-08-09 23:11:35 -0500

    beer

--- client/aspects/alcohol.lua
+++ client/aspects/alcohol.lua
@@ -1,3 +1,4 @@
+-- rancho beer box
 exports('box_beer_rancho', function()
     local crds = GetOffsetFromEntityInWorldCoords(cache.ped, 0.0, 0.6, 0.0)
     local head = GetEntityHeading(cache.ped)
@@ -9,7 +10,7 @@ exports('box_beer_rancho', function()
     lib.callback('miut:server:remove:ranchobox')
     if lib.progressBar({
         duration = 5000,
-        label = 'Opening MRE',
+        label = 'Opening Pack of Rancho Beers',
         useWhileDead = false,
         canCancel = true,
         disable = {
@@ -20,8 +21,37 @@ exports('box_beer_rancho', function()
             scenario = 'CODE_HUMAN_MEDIC_TEND_TO_DEAD',
         },
     }) then
-        lib.callback('miut:server:add:mre')
+        lib.callback('miut:server:add:beer_rancho')
     else
-        lib.callback('miut:server:canceled:mre')
+        lib.callback('miut:server:canceled:beer_rancho')
+    end
+end)
+
+-- dusche beer box
+exports('box_beer_rancho', function()
+    local crds = GetOffsetFromEntityInWorldCoords(cache.ped, 0.0, 0.6, 0.0)
+    local head = GetEntityHeading(cache.ped)
+    local box = lib.callback('miut:server:spawnbox:small', false, function() end, crds)
+    SetEntityHeading(box, head)
+    PlaceObjectOnGroundProperly(box)
+    FreezeEntityPosition(box, true)
+    SetEntityCollision(box, true, true)
+    lib.callback('miut:server:remove:beer_dusche')
+    if lib.progressBar({
+        duration = 5000,
+        label = 'Opening Pack of Rancho Beers',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+            move = true
+        },
+        anim = {
+            scenario = 'CODE_HUMAN_MEDIC_TEND_TO_DEAD',
+        },
+    }) then
+        lib.callback('miut:server:add:beer_dusche')
+    else
+        lib.callback('miut:server:canceled:beer_dusche')
     end
 end)
--- server/aspects/alcohol.lua
+++ server/aspects/alcohol.lua
@@ -1,4 +1,5 @@
-lib.callback.register('miut:server:remove:mre', function(source)
+-- rancho beer box
+lib.callback.register('miut:server:remove:beer_rancho', function(source)
     local hasitem = Inventory:GetItem(source, 'beer_rancho_box', 1, true)
     if hasitem then
         exports.ox_inventory:RemoveItem(source, 'beer_rancho_box', 1)
@@ -7,6 +8,28 @@ lib.callback.register('miut:server:remove:mre', function(source)
     end
 end)
 
-lib.callback.register('miut:server:add:mre', function(source)
+lib.callback.register('miut:server:add:beer_rancho', function(source)
     exports.ox_inventory:AddItem(source, 'beer_rancho', UT.alcohol.rancho)
+end)
+
+lib.callback.register('miut:server:canceled:beer_rancho', function(source)
+    exports.ox_inventory:AddItem(source, 'beer_rancho_box', UT.alcohol.rancho)
+end)
+
+-- dusche beer box
+lib.callback.register('miut:server:remove:beer_dusche', function(source)
+    local hasitem = Inventory:GetItem(source, 'box_dusche_beer', 1, true)
+    if hasitem then
+        exports.ox_inventory:RemoveItem(source, 'box_dusche_beer', 1)
+    else
+        print('nyopendedoor')
+    end
+end)
+
+lib.callback.register('miut:server:add:beer_dusche', function(source)
+    exports.ox_inventory:AddItem(source, 'beer_dusche', UT.alcohol.dusche)
+end)
+
+lib.callback.register('miut:server:canceled:beer_dusche', function(source)
+    exports.ox_inventory:AddItem(source, 'beer_dusche', UT.alcohol.dusche)
 end)
